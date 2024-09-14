d8a51cdf1c2c5dcd191101c48644b462bdf66a9d
Fernando Messer <fernandomesser88@gmail.com>
2024-09-14 17:54:30 -0600

    Started Rook, copied code from bishop to use it as a starter

---
+++ shared/src/main/java/chess/RookMovesCalculator.java
@@ -0,0 +1,44 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class RookMovesCalculator implements PieceMovesCalculator {
+    private static final int[][] MOVES = {
+        {1, 0},{-1, 0},{0, 1},{0, -1}
+    };
+    @Override
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        Collection<ChessMove> validMoves = new ArrayList<>();
+
+        for(int[] move: MOVES){
+            var checkRow = myPosition.getRow();
+            var checkCol = myPosition.getColumn();
+
+            while (true){
+                checkRow += move[0];
+                checkCol += move[1];
+
+                if(!inBound(checkRow, checkCol)){break;}
+                ChessPosition newPosition = new ChessPosition(checkRow,checkCol);
+
+                if (board.getPiece(newPosition)==null){
+                    validMoves.add(new ChessMove(myPosition, newPosition, null));
+                }else if (board.getPiece(newPosition).getTeamColor() != board.getPiece(myPosition).getTeamColor()){
+                    validMoves.add(new ChessMove(myPosition, newPosition, null));
+                    break;
+                }else{
+                    break;
+                }
+                {
+
+                }
+            }
+
+        }
+        return validMoves;
+    }
+    private boolean inBound(int row, int col) {
+        return (row > 0 && row < 9) && (col > 0 && col < 9);
+    }
+}
