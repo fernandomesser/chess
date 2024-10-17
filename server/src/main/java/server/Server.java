2a3e0ebf4c7b7eff517196ff338e2430d0fd540c
Fernando Messer <fernandomesser88@gmail.com>
2024-10-17 10:07:57 -0600

    Start imp of Server

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,5 +1,6 @@
 package server;
 
+import exception.ResponseException;
 import spark.*;
 
 public class Server {
@@ -10,12 +11,53 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.post("/user",this::register);
+        Spark.post("/session",this::logIn);
+        Spark.delete("/session",this::logOut);
+        Spark.get("/game", this::listGames);
+        Spark.post("/game",this::createGame);
+        Spark.put("/game",this::joinGame);
+        Spark.delete("/db",this::clearApp);
+        Spark.exception(ResponseException.class, this::exceptionHandler);
 
-        //This line initializes the server and can be removed once you have a functioning endpoint 
+
+        //This line initializes the server and can be removed once you have a functioning endpoint
         Spark.init();
 
         Spark.awaitInitialization();
         return Spark.port();
+    }
+
+    private <T extends Exception> void exceptionHandler(ResponseException ex, Request req, Response res) {
+        res.status(ex.StatusCode());
+    }
+
+    private Object register(Request req, Response res) throws ResponseException {
+        return null;
+    }
+
+    private Object logIn(Request req, Response res) throws ResponseException {
+        return null;
+    }
+
+    private Object logOut(Request req, Response res) throws ResponseException {
+        return null;
+    }
+
+    private Object listGames(Request req, Response res) throws ResponseException {
+        return null;
+    }
+
+    private Object createGame(Request req, Response res) throws ResponseException {
+        return null;
+    }
+
+    private Object joinGame(Request req, Response res) throws ResponseException {
+        return null;
+    }
+
+    private Object clearApp(Request req, Response res) throws ResponseException {
+        return null;
     }
 
     public void stop() {
--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,4 +1,22 @@
 package service;
 
+import dataaccess.AuthDAO;
+import dataaccess.MemoryUserDAO;
+import dataaccess.UserDAO;
+import exception.ResponseException;
+import model.UserData;
+import passoff.exception.ResponseParseException;
+
 public class UserService {
+    private final UserDAO userDataAccess;
+
+    public UserService(UserDAO userDataAccess) {
+        this.userDataAccess = userDataAccess;
+    }
+
+    public UserData register(UserData user) throws ResponseException {
+
+
+        return null;
+    }
 }
