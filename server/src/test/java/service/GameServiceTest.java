a65f096d6baf1357b3d2f2fac31371ae69527358
Fernando Messer <fernandomesser88@gmail.com>
2024-10-18 18:28:29 -0600

    Starting Tests

---
+++ server/src/test/java/service/GameServiceTest.java
@@ -0,0 +1,24 @@
+package java.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameServiceTest {
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void createGame() {
+    }
+
+    @Test
+    void listGames() {
+    }
+
+    @Test
+    void joinGame() {
+    }
+}
---
+++ server/src/test/java/service/UserServiceTest.java
@@ -0,0 +1,24 @@
+package java.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void logIn() {
+    }
+
+    @Test
+    void logOut() {
+    }
+}
