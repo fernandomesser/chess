6946d803cb226dedd42da6b21c89ef944b1099a7
Fernando Messer <fernandomesser88@gmail.com>
2024-10-29 10:42:27 -0600

    create auth

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,13 +1,14 @@
 package dataaccess;
 
+import exception.ResponseException;
 import model.AuthData;
 
 public interface AuthDAO {
-    AuthData createAuth(String username) throws DataAccessException;
+    AuthData createAuth(String username) throws DataAccessException, ResponseException;
 
-    AuthData getAuth(String authToken) throws DataAccessException;
+    AuthData getAuth(String authToken) throws DataAccessException, ResponseException;
 
-    void deleteAuth(String authToken) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException, ResponseException;
 
-    void clearAuth() throws DataAccessException;
+    void clearAuth() throws DataAccessException, ResponseException;
 }
--- server/src/main/java/dataaccess/SqlAuthDAO.java
+++ server/src/main/java/dataaccess/SqlAuthDAO.java
@@ -1,5 +1,6 @@
 package dataaccess;
 
+import java.util.UUID;
 import exception.ResponseException;
 import model.AuthData;
 
@@ -7,18 +8,22 @@ public class SqlAuthDAO extends BaseSqlDAO implements AuthDAO{
 
     private static final String[] CREATE_STATEMENTS = {
             """
-            CREATE TABLE IF NOT EXISTS  auth (
-               authtoken VARCHAR(256) NOT NULL PRIMARY KEY,
-               username VARCHAR(256) NOT NULL,
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            CREATE TABLE IF NOT EXISTS auth (
+                authToken VARCHAR(36) NOT NULL PRIMARY KEY,
+                username VARCHAR(256) NOT NULL,
+                FOREIGN KEY (username) REFERENCES users(username) ON DELETE CASCADE
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
             """
     };
     public SqlAuthDAO(){
         super(CREATE_STATEMENTS);
     }
     @Override
-    public AuthData createAuth(String username) throws DataAccessException {
-        return null;
+    public AuthData createAuth(String username) throws ResponseException {
+        String authToken = UUID.randomUUID().toString();
+        String insertStatement = "INSERT INTO auth (authToken, username) VALUES (?, ?)";
+        executeUpdate(insertStatement, authToken, username);
+        return new AuthData(authToken, username);
     }
 
     @Override
