b14aa551be3afd3dac7d48fe04889bc5434e9b37
Fernando Messer <fernandomesser88@gmail.com>
2024-10-29 10:26:22 -0600

    Base DAO class to avoid duplication

---
+++ server/src/main/java/dataaccess/BaseSqlDAO.java
@@ -0,0 +1,42 @@
+package dataaccess;
+
+import exception.ResponseException;
+
+import java.sql.SQLException;
+
+public abstract class BaseSqlDAO {
+
+    protected int executeUpdate(String statement, Object... params) throws ResponseException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    if (param instanceof String p) {
+                        ps.setString(i + 1, p);
+                    } else if (param instanceof Integer p) {
+                        ps.setInt(i + 1, p);
+                    } else if (param == null) {
+                        ps.setNull(i + 1, java.sql.Types.NULL);
+                    }
+                }
+                return ps.executeUpdate();
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new ResponseException(500, String.format("Unable to update database: %s, %s", statement, e.getMessage()));
+        }
+    }
+
+    protected void configureDatabase(String[] createStatements) throws ResponseException, DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new ResponseException(500, String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
+}
+
--- server/src/main/java/dataaccess/SqlUserDAO.java
+++ server/src/main/java/dataaccess/SqlUserDAO.java
@@ -1,17 +1,16 @@
 package dataaccess;
 
-import com.google.gson.Gson;
 import exception.ResponseException;
 import model.UserData;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class SqlUserDAO implements UserDAO {
+public class SqlUserDAO extends BaseSqlDAO implements UserDAO {
 
     public SqlUserDAO() {
         try {
-            configureDatabase();
+            configureDatabase(createStatements);
         } catch (ResponseException | DataAccessException e) {
             throw new RuntimeException("Failed to initialize SqlUserDAO: " + e.getMessage(), e);
         }
@@ -54,26 +53,6 @@ public class SqlUserDAO implements UserDAO {
         executeUpdate(statement);
     }
 
-    private int executeUpdate(String statement, Object... params) throws ResponseException {
-        try (var conn = DatabaseManager.getConnection()) {
-            try (var ps = conn.prepareStatement(statement)) {
-                for (var i = 0; i < params.length; i++) {
-                    var param = params[i];
-                    if (param instanceof String p) {
-                        ps.setString(i + 1, p);
-                    } else if (param instanceof Integer p) {
-                        ps.setInt(i + 1, p);
-                    } else if (param == null) {
-                        ps.setNull(i + 1, java.sql.Types.NULL);
-                    }
-                }
-                return ps.executeUpdate();
-            }
-        } catch (SQLException | DataAccessException e) {
-            throw new ResponseException(500, String.format("Unable to update database: %s, %s", statement, e.getMessage()));
-        }
-    }
-
     private final String[] createStatements = {
             """
             CREATE TABLE IF NOT EXISTS  users (
@@ -83,17 +62,4 @@ public class SqlUserDAO implements UserDAO {
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
             """
     };
-    private void configureDatabase() throws ResponseException, DataAccessException {
-        DatabaseManager.createDatabase();
-        try (var conn = DatabaseManager.getConnection()) {
-            for (var statement : createStatements) {
-                try (var preparedStatement = conn.prepareStatement(statement)) {
-                    preparedStatement.executeUpdate();
-                }
-            }
-        } catch (SQLException ex) {
-            throw new ResponseException(500, String.format("Unable to configure database: %s", ex.getMessage()));
-        }
-    }
-
 }
