de7facf43d29994a6413b25efa909cb949822f60
Fernando Messer <fernandomesser88@gmail.com>
2024-11-09 14:20:42 -0700

    REPL

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -1,6 +1,9 @@
 package ui;
 
 
+import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
 
 public class Repl {
     private final ChessClient client;
@@ -8,4 +11,30 @@ public class Repl {
     public Repl(String serverUrl) {
         client = new ChessClient(serverUrl);
     }
+
+    public void run() {
+        System.out.println(BLACK_QUEEN + "Welcome to Chess. Sign in to start." + BLACK_QUEEN);
+        System.out.print(client.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = client.eval(line);
+                System.out.print(SET_TEXT_COLOR_BLUE + result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+    private void printPrompt() {
+        System.out.print("\n" + RESET_TEXT_COLOR + ">>> " + SET_TEXT_COLOR_GREEN);
+    }
+
 }
