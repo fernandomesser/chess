6645809073aba4ebaea3a344ec966f3009959b8d
Fernando Messer <fernandomesser88@gmail.com>
2024-11-08 17:27:28 -0700

    Beggining imp of Client

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -1,5 +1,10 @@
 package ui;
 
+import exception.ResponseException;
+import model.UserData;
+
+import java.util.Arrays;
+
 public class ChessClient {
     private String visitorName = null;
     private final ServerFacade server;
@@ -11,5 +16,58 @@ public class ChessClient {
         this.serverUrl = serverUrl;
     }
 
-    
+    public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "register" -> register();
+                case "login" -> logIn();
+                case "logout" -> logOut();
+                case "list" -> listGames();
+                case "create" -> createGame();
+                case "join" -> joinGame();
+                case "observe" -> observeGame();
+                case "quit" -> "quit";
+                default -> help();
+            };
+        } catch (ResponseException ex) {
+            return ex.getMessage();
+        }
+    }
+
+    private String register() {
+        return null;
+    }
+
+    private String logIn() {
+        return null;
+    }
+
+    private String logOut() {
+        return null;
+    }
+
+    private String listGames() {
+        return null;
+    }
+
+    private String createGame() {
+        return null;
+    }
+
+    private String joinGame() {
+        return null;
+    }
+
+    private String observeGame() {
+        return null;
+    }
+
+    private String help() {
+        return null;
+    }
+
+
 }
