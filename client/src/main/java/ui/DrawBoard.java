ec3f0021e0826c3ccc1545a139cc0438c58a4b85
Fernando Messer <fernandomesser88@gmail.com>
2024-11-12 10:49:40 -0700

    draw headers wpi

--- client/src/main/java/ui/DrawBoard.java
+++ client/src/main/java/ui/DrawBoard.java
@@ -12,6 +12,7 @@ import static ui.EscapeSequences.*;
 
 public class DrawBoard {
     private static final ChessGame game = new ChessGame();
+    private static final String view = "WHITE";
 
 //    public DrawBoard(ChessGame game) {
 //        this.game = game;
@@ -25,20 +26,10 @@ public class DrawBoard {
     }
 
     private static void printBoard(PrintStream out) {
-        for (int i = 0; i < 8; i++) {
-            int i0 = i % 2 == 0 ? 1 : 0;
-            int i1 = i % 2 == 0 ? 0 : 1;
-            for (int j = 0; j < 8; j++) {
-                if (j % 2 == i0) {
-                    out.print(SET_BG_COLOR_BLACK);
-                    printPiece(i + 1, j + 1, out);
-                    out.print(RESET_BG_COLOR);
-                }
-                if (j % 2 == i1) {
-                    out.print(SET_BG_COLOR_WHITE);
-                    printPiece(i + 1, j + 1, out);
-                    out.print(RESET_BG_COLOR);
-                }
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                drawHeaders(out, i, j);
+                drawBoard(out, i, j);
             }
             out.println();
         }
@@ -46,67 +37,44 @@ public class DrawBoard {
 
 
     private static void printPiece(int i, int j, PrintStream out) {
+        String textColor = view.equals("WHITE") ? SET_TEXT_COLOR_WHITE : SET_TEXT_COLOR_BLACK;
         String piece = getPiece(i, j);
         switch (piece) {
             case "P" -> {
-                out.print(SET_TEXT_COLOR_BLACK);
-                System.out.print(BLACK_PAWN);
-                out.print(RESET_TEXT_COLOR);
+                pieceColor(out, BLACK_PAWN, SET_TEXT_COLOR_BLACK);
             }
             case "R" -> {
-                out.print(SET_TEXT_COLOR_BLACK);
-                System.out.print(BLACK_ROOK);
-                out.print(RESET_TEXT_COLOR);
+                pieceColor(out, BLACK_ROOK, SET_TEXT_COLOR_BLACK);
             }
             case "K" -> {
-                out.print(SET_TEXT_COLOR_BLACK);
-                System.out.print(BLACK_KING);
-                out.print(RESET_TEXT_COLOR);
+                pieceColor(out, BLACK_KING, SET_TEXT_COLOR_BLACK);
             }
             case "Q" -> {
-                out.print(SET_TEXT_COLOR_BLACK);
-                System.out.print(BLACK_QUEEN);
-                out.print(RESET_TEXT_COLOR);
+                pieceColor(out, BLACK_QUEEN, SET_TEXT_COLOR_BLACK);
             }
             case "B" -> {
-                out.print(SET_TEXT_COLOR_BLACK);
-                System.out.print(BLACK_BISHOP);
-                out.print(RESET_TEXT_COLOR);
+                pieceColor(out, BLACK_BISHOP, SET_TEXT_COLOR_BLACK);
             }
             case "N" -> {
-                out.print(SET_TEXT_COLOR_BLACK);
-                System.out.print(BLACK_KNIGHT);
-                out.print(RESET_TEXT_COLOR);
+                pieceColor(out, BLACK_KNIGHT, SET_TEXT_COLOR_BLACK);
             }
             case "p" -> {
-                out.print(SET_TEXT_COLOR_WHITE);
-                System.out.print(BLACK_PAWN);
-                out.print(RESET_TEXT_COLOR);
+                pieceColor(out, BLACK_PAWN, SET_TEXT_COLOR_WHITE);
             }
             case "r" -> {
-                out.print(SET_TEXT_COLOR_WHITE);
-                out.print(RESET_TEXT_COLOR);
-                System.out.print(BLACK_ROOK);
+                pieceColor(out, BLACK_ROOK, SET_TEXT_COLOR_WHITE);
             }
             case "k" -> {
-                out.print(SET_TEXT_COLOR_WHITE);
-                System.out.print(BLACK_KING);
-                out.print(RESET_TEXT_COLOR);
+                pieceColor(out, BLACK_KING, SET_TEXT_COLOR_WHITE);
             }
             case "q" -> {
-                out.print(SET_TEXT_COLOR_WHITE);
-                System.out.print(BLACK_QUEEN);
-                out.print(RESET_TEXT_COLOR);
+                pieceColor(out, BLACK_QUEEN, SET_TEXT_COLOR_WHITE);
             }
             case "b" -> {
-                out.print(SET_TEXT_COLOR_WHITE);
-                System.out.print(BLACK_BISHOP);
-                out.print(RESET_TEXT_COLOR);
+                pieceColor(out, BLACK_BISHOP, SET_TEXT_COLOR_WHITE);
             }
             case "n" -> {
-                out.print(SET_TEXT_COLOR_WHITE);
-                System.out.print(BLACK_KNIGHT);
-                out.print(RESET_TEXT_COLOR);
+                pieceColor(out, BLACK_KNIGHT, SET_TEXT_COLOR_WHITE);
             }
             default -> {
                 System.out.print(EMPTY);
@@ -118,6 +86,39 @@ public class DrawBoard {
         ChessBoard board = game.getBoard();
         ChessPiece piece = board.getPiece(new ChessPosition(i, j));
         return piece == null ? " " : piece.toString();
+    }
+
+    private static void pieceColor(PrintStream out, String piece, String color) {
+        out.print(color);
+        System.out.print(piece);
+        out.print(RESET_TEXT_COLOR);
+    }
+
+
+    private static void drawBoard(PrintStream out, int i, int j) {
+        int i0 = i % 2 == 0 ? 1 : 0;
+        int i1 = i % 2 == 0 ? 0 : 1;
+        if (i < 9 && j < 9 && i > 0 && j > 0) {
+            if (j % 2 == i0) {
+                out.print(SET_BG_COLOR_RED);
+                printPiece(i, j, out);
+                out.print(RESET_BG_COLOR);
+            }
+            if (j % 2 == i1) {
+                out.print(SET_BG_COLOR_LIGHT_GREY);
+                printPiece(i, j, out);
+                out.print(RESET_BG_COLOR);
+            }
+        }
+    }
+
+    private static void drawHeaders(PrintStream out, int i, int j) {
+        if ((i == 0 || i == 9)||(j==0||j==9)) {
+            out.print(SET_BG_COLOR_DARK_GREEN);
+            System.out.print(EMPTY);
+            out.print(RESET_BG_COLOR);
+        }
+
     }
 
 }
