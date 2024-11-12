bd100187337c32aa2c97fe3fbb56d3d181081698
Fernando Messer <fernandomesser88@gmail.com>
2024-11-12 08:08:46 -0700

    Board skeleton

--- client/src/main/java/ui/DrawBoard.java
+++ client/src/main/java/ui/DrawBoard.java
@@ -1,13 +1,37 @@
 package ui;
+
 import static ui.EscapeSequences.*;
 
-public class DrawBoard {
-    // Board dimensions.
-    private static final int BOARD_SIZE_IN_SQUARES = 8;
-    private static final int SQUARE_SIZE_IN_PADDED_CHARS = 8;
-    private static final int LINE_WIDTH_IN_PADDED_CHARS = 1;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
 
-    // Padded characters.
-    private static final String EMPTY = "   ";
+public class DrawBoard {
+    public static void main(String[] args) {
+        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+        out.print(ERASE_SCREEN);
+        printBoard(out);
+
+    }
+
+    private static void printBoard(PrintStream out){
+        for (int i = 0; i < 8; i++) {
+            int i0 = i % 2 == 0 ? 1 : 0;
+            int i1 = i % 2 == 0 ? 0 : 1;
+            for (int j = 0; j < 8; j++) {
+                if (j % 2 == i0) {
+                    out.print(SET_BG_COLOR_BLACK);
+                    System.out.print(EMPTY);
+                    out.print(RESET_BG_COLOR);
+                }
+                if (j % 2 == i1) {
+                    out.print(SET_BG_COLOR_WHITE);
+                    System.out.print(EMPTY);
+                    out.print(RESET_BG_COLOR);
+                }
+            }
+            out.println();
+        }
+    }
 
 }
